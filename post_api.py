883b209345bf3d970825150726e69ab44a35bdcc
Sergio Sierra <sergiosierra@cleta.io>
2021-11-19 20:03:36 -0500

    Added unittest file. ApiData class implementation start.

--- post_api.py
+++ post_api.py
@@ -1,6 +1,7 @@
 # Simple post api using aiohttp and asyncio
 import aiohttp
 import asyncio
+import json
 
 async def post_call():
     url = "https://ancient-wood-1161.getsandbox.com:443/results"
@@ -16,8 +17,35 @@ async def post_call():
 
 async def do_simple_post(session, url, arg=None):
     async with session.post(url, json={}) as response:
-          data = await response.text()
-          print (data)
+        data = await response.text()
+        print(data)
+        #print(type(data))  # String
+        chronological_order(data, "publishedDate")
+
+
+def chronological_order(data, key, reverse=True):
+    '''
+    @param data: Data to be ordered
+    @param key: Key on which to perform the order operation
+    @param reverse: Set to true to enable reverse chronological order
+    '''
+    print("Ordering data by key {} in reverse order = {}".format(key, reverse))
+    print(type(data))
+    # For this specific api the key is publicationDate
+
+    def extract_time(data, key):
+        for sport in data:
+            try:
+                # Also convert to int since update_time will be string.  When comparing
+                # strings, "10" is smaller than "2".
+                return int(json[sport][key])
+            except KeyError:
+                return 0
+    # sort by extract time
+    data = json.loads(data)
+    print("here")
+    print(type(data))
+    return data.sort(key=extract_time, reverse=reverse)
 
 async def do_post(session, url, x):
     async with session.post(url, data ={
